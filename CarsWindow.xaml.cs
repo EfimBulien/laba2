83b37fb395fb4101f12ef471188cda2b93318b73
EfimBulien <isip_e.m.bulien@mpt.ru>
2024-03-16 17:49:27 +0300

    first commit

---
+++ CarsWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using laba2.lab1DataSetTableAdapters;
+using System;
+using System.Data;
+using System.Windows;
+
+namespace laba2
+{
+    public partial class CarsWindow : Window
+    {
+        carsTableAdapter carsAdapter = new carsTableAdapter();
+        public CarsWindow()
+        {
+            InitializeComponent();
+            CarsData.ItemsSource = carsAdapter.GetData();
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            carsAdapter.InsertQuery(modelBox.Text, numberBox.Text, colorBox.Text);
+            RefreshTable();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CarsData.SelectedItem != null)
+            {
+                object id = (CarsData.SelectedItem as DataRowView).Row[0];
+                carsAdapter.UpdateQuery(modelBox.Text, numberBox.Text, colorBox.Text, Convert.ToInt32(id));
+                RefreshTable();
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CarsData.SelectedItem != null)
+            {
+                object id = (CarsData.SelectedItem as DataRowView).Row[0];
+                carsAdapter.DeleteQuery(Convert.ToInt32(id));
+                RefreshTable();
+            }
+        }
+
+        private void RefreshTable()
+        {
+            CarsData.ItemsSource = carsAdapter.GetData();
+        }
+    }
+}
---
+++ DriversWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using laba2.lab1DataSetTableAdapters;
+using System;
+using System.Data;
+using System.Windows;
+
+namespace laba2
+{
+    public partial class DriversWindow : Window
+    {
+        driversTableAdapter driversAdapter = new driversTableAdapter();
+        public DriversWindow()
+        {
+            InitializeComponent();
+            DriversData.ItemsSource = driversAdapter.GetData();
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            var current_car_id = Convert.ToInt32(car_idBox.Text);
+            driversAdapter.InsertQuery(firstnameBox.Text, surnameBox.Text, middlenameBox.Text, current_car_id);
+            RefreshTable();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DriversData.SelectedItem != null)
+            {
+                var current_car_id = Convert.ToInt32(car_idBox.Text);
+                object id = (DriversData.SelectedItem as DataRowView).Row[0];
+                driversAdapter.UpdateQuery(firstnameBox.Text, surnameBox.Text, middlenameBox.Text, current_car_id, Convert.ToInt32(id));
+                RefreshTable();
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DriversData.SelectedItem != null)
+            {
+                object id = (DriversData.SelectedItem as DataRowView).Row[0];
+                driversAdapter.DeleteQuery(Convert.ToInt32(id));
+                RefreshTable();
+            }
+        }
+
+        private void RefreshTable()
+        {
+            DriversData.ItemsSource = driversAdapter.GetData();
+        }
+    }
+}
